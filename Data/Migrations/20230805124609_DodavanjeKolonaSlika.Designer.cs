fbbaee1335d0e2c7f13e5f118fad26c80f230985
dominikpavlic <124397067+dominikpavlic@users.noreply.github.com>
2023-08-08 20:18:37 +0200

    dodavanje kolone slika
    dodavanje kolone slika u tablicu proizvod - codefirst pristup

--- Data/Migrations/20230805124609_DodavanjeKolonaSlika.Designer.cs
+++ Data/Migrations/20230805124609_DodavanjeKolonaSlika.Designer.cs
@@ -4,6 +4,7 @@ using CMSProductSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMSProductSystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230805124609_DodavanjeKolonaSlika")]
+    partial class DodavanjeKolonaSlika
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -142,8 +145,8 @@ namespace CMSProductSystem.Data.Migrations
                     b.Property<int>("CategoryID")
                         .HasColumnType("int");
 
-                    b.Property<decimal>("Cijena")
-                        .HasColumnType("decimal(18,2)");
+                    b.Property<double>("Cijena")
+                        .HasColumnType("float");
 
                     b.Property<int>("Kolicina")
                         .HasColumnType("int");
---
+++ Data/Migrations/20230805133843_PromjenaTipaKoloneCijena.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMSProductSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PromjenaTipaKoloneCijena : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slika",
+                table: "Proizvod",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Cijena",
+                table: "Proizvod",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Slika",
+                table: "Proizvod",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Cijena",
+                table: "Proizvod",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
