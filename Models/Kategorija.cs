3b50076dbaf9ce7b0ab0b8de08e7a8218003d41a
dominikpavlic <124397067+dominikpavlic@users.noreply.github.com>
2023-08-10 16:04:22 +0200

    promjena prikaza kategorije

--- Models/Kategorija.cs
+++ Models/Kategorija.cs
@@ -1,8 +1,13 @@
-﻿namespace CMSProductSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMSProductSystem.Models
 {
     public class Kategorija
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Naziv je obavezan")]
+        [Display(Name = "Naziv")]
         public string Naziv { get; set; }
 
         public ICollection<Proizvod> Product { get; set; } = new List<Proizvod>();
