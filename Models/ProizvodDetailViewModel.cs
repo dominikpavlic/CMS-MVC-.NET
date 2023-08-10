e144b5f2eaa4c42f5f05fbc1d68e735699bd907e
dominikpavlic <124397067+dominikpavlic@users.noreply.github.com>
2023-08-10 16:06:20 +0200

    nova klasa za API endpoint
    nova klasa za prikaz podataka za API endpoint

---
+++ Models/ProizvodDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CMSProductSystem.Models
+{
+    public class ProizvodDetailViewModel
+    {
+        public int ID { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public int Kolicina { get; set; }
+        public decimal Cijena { get; set; }
+        public string KategorijaNaziv { get; set; }
+
+        public string? Slika { get; set; }
+    }
+}
